00aa8e79e53a97071b4c14ed9bdec4a3e95e814c
Bogdan Orzea <bogdan.orzea@gmail.com>
2017-07-13 14:30:19 +0300

    Improve layout for the EditorActivity

--- app/src/main/java/com/bogdanorzea/inventoryapp/ProductEditorActivity.java
+++ app/src/main/java/com/bogdanorzea/inventoryapp/ProductEditorActivity.java
@@ -2,6 +2,7 @@ package com.bogdanorzea.inventoryapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
 public class ProductEditorActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class ProductEditorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_editor);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
     }
 }
